5f67a1a5e426ba9ebe3a13cfa35cb479d9010b13
ThatGuy <thatguygorillatag@gmail.com>
2025-04-20 17:23:14 -0700

    Delete TTSGUI directory

--- TTSGUI/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TTSGUI
-{
-    internal class PluginInfo
-    {
-        public const string GUID = "com.goldentrophy.gorillatag.ttsgui";
-        public const string Name = "TTSGUI";
-        public const string Version = "1.0.0";
-    }
-}
